ec4debf10088af26316ef700a898563ed337598f
SebFunch <sebastian.funch@gmail.com>
2017-03-22 10:07:54 +0100

    Created board class
    Created board class

---
+++ src/main/Board.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Board {
+	
+	private ArrayList<Boat> boats;
+	private int[][] board;
+	private char hit;
+	private char missed;
+	private char boat;
+	
+	public Board(int length, int height) {
+		
+	}
+	
+}
