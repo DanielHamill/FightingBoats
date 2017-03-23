61cf4a653761b18b66c356b23695659821f3cd76
SebFunch <sebastian.funch@gmail.com>
2017-03-23 12:49:18 +0100

    Made game class

---
+++ src/main/Game.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Game {
+	
+	private boolean running;
+	private String hostName;
+	
+	public void setUp(){
+		
+	}
+	
+	public void mainLoop(){
+		
+	}
+	
+	public void AIMove(){
+		
+	}
+	
+}
