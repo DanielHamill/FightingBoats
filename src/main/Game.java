02cc34a5a8e15970766b3fadfdf350c276c6e8a0
DanielHamill <DanielHamill00@gmail.com>
2017-04-28 07:03:38 +0200

    comments

--- src/main/Game.java
+++ src/main/Game.java
@@ -40,14 +40,17 @@ public class Game {
 	Boat.boatType[] arr;
 
 	public Game() {
+		//initialize game elements
 		enemyBoard = new Board(BOARD_SIZE,BOARD_SIZE);
 		ownBoard = new Board(BOARD_SIZE,BOARD_SIZE);
 		boats = new ArrayList<Boat>();
 
+		//game utilities
 		scanner = new Scanner(System.in);
 		boatToPlace = 0;
 		arr = new Boat.boatType[]{boatType.BIG_OL_BOAT, boatType.PLANE_HOLDY_BOAT, boatType.LIL_TINY_BOAT};
 
+		//game data
 		running = true;
 		setup = false;
 		gameOver = false;
@@ -58,12 +61,14 @@ public class Game {
 	public void mainLoop(){
 		while(running) {
 			try{
+				//main game loop
 				if(!setup)
 					setUp();
 				else if(!gameOver)
 					game();
 				else
 					end();
+				//Exception catching
 			} catch(ArrayIndexOutOfBoundsException e) {
 				System.out.println("That boat does not fit on the board. Try again");
 //				e.printStackTrace();
@@ -77,15 +82,18 @@ public class Game {
 	}
 
 	public void setUp() throws IOException, NumberFormatException{
+		//setup, handles connection, and board setup
 		if(boats.size()>=BOAT_AMOUNT) {
 			setup = true;
 			return;
 		}
 
 		if(host==null) {
+			//setting up the sockets and connecting the players
 			System.out.println("Are you hosting this game? (y/n)");
 			String raw = scanner.nextLine();
 			if(raw.equals("y")) {
+				//handles setup for the server
 				host = "hosting";
 				socket = new ServerSocket(8888).accept();
 
@@ -98,6 +106,7 @@ public class Game {
 				else send("a");
 			}
 			else if(raw.equals("n")){
+				//handes setup for the client
 				System.out.println("What is the adress you would like to connect to?");
 				host = scanner.nextLine();
 				socket = new Socket(host,8888);
@@ -115,16 +124,20 @@ public class Game {
 			}
 
 		}
+		//places boats
 		if(boatToPlace==0) ownBoard.drawBoard();
-//		if(boatToPlace<BOAT_AMOUNT) setup = false;
 		String raw = "";
 		int x = 0;
 		int y = 0;
+		
+		//keeps track of the boats that need to be placed
 		int boatIndex = boatToPlace;
 		if(boatToPlace>=arr.length) boatIndex = arr.length-1;
 		String s = boatToPlace < BOAT_AMOUNT-1 ? "s " : " ";
 		System.out.println("You have " + (BOAT_AMOUNT-boatToPlace) + " boat" + s + "to place.");
 		System.out.println("Where would you like to place the " + Boat.BOAT_INFO[boatIndex*2] + "? " + Boat.BOAT_INFO[boatIndex*2+1]);
+		
+		//gets coordinates from player
 		raw = scanner.nextLine();
 		int charIndex = 0;
 		for(int i=0; i<raw.length(); i++) {
@@ -136,6 +149,7 @@ public class Game {
 		x = (int)Character.toUpperCase(raw.charAt(0))-65;
 		y = Integer.parseInt(raw.substring(charIndex,raw.length()))-1;
 
+		//creates boat object and places on board
 		System.out.println("Would you like the boat vertical or horizontal? (v/h)");
 		raw = scanner.nextLine();
 		boolean vertical = true;
@@ -146,6 +160,7 @@ public class Game {
 		for(int i = newBoat.xPos(); i < newBoat.xPos()+newBoat.LENGTH; i++) {
 			for(int j = newBoat.yPos(); j < newBoat.yPos()+newBoat.WIDTH; j++) {
 				if(hitBoat(i,j)) {
+					//ERROR CHECKING: returns from method in loop
 					System.out.println("Sorry, there is already a boat there. Place it somewhere else");
 					return;
 				}
@@ -160,7 +175,9 @@ public class Game {
 	}
 
 	public void game() throws IOException {
+		//main game loop between users
 		if(attacking) {
+			//handles attacking
 			System.out.println("Where would you like to attack?");
 			String raw = scanner.nextLine();
 			int charIndex = 0;
@@ -173,10 +190,10 @@ public class Game {
 			int x = (int)Character.toUpperCase(raw.charAt(0))-65;
 			int y = Integer.parseInt(raw.substring(1,raw.length()))-1;
 			
-			// ERROR CHECKING - OFFBOARD
+			// ERROR CHECKING: OFFBOARD
 			char ch = ownBoard.getBoard()[x][y];
 			
-			// ERROR CHECKING - ALREADY ATTACKED
+			// ERROR CHECKING: ALREADY ATTACKED
 			if(enemyBoard.getBoard()[x][y] == 'x' || enemyBoard.getBoard()[x][y] == 'o') {
 				System.out.println("You already attacked there, try again");
 				return;
@@ -198,6 +215,7 @@ public class Game {
 			enemyBoard.drawBoard();
 		}
 		else {
+			//handes receiving
 			System.out.println("Wait for opponent");
 			String raw = recieve();
 			int x = Integer.parseInt(raw.substring(0,raw.indexOf(",")));
